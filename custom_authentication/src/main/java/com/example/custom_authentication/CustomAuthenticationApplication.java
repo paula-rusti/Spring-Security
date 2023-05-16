99f10107c9220590fe487b0c21a2949f468bf79b
Paula Rusti <rusti.paula78@gmail.com>
2023-05-16 18:38:25 +0300

    custom authentication initial commit

---
+++ custom_authentication/src/main/java/com/example/custom_authentication/CustomAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAuthenticationApplication.class, args);
+	}
+
+}
