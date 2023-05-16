9ab9d434824036256269c046d52d88fc726f7899
Paula Rusti <rusti.paula78@gmail.com>
2023-05-16 23:36:34 +0300

    remade custom authentication with maven

--- custom_authentication/src/main/java/com/example/custom_authentication/CustomAuthenticationApplication.java
+++ custom_authentication/src/main/java/com/example/custom_authentication/CustomAuthenticationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomAuthenticationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomAuthenticationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomAuthenticationApplication.class, args);
+    }
 
 }
--- custom_authentication/src/test/java/com/example/custom_authentication/CustomAuthenticationApplicationTests.java
+++ custom_authentication/src/test/java/com/example/custom_authentication/CustomAuthenticationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CustomAuthenticationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
