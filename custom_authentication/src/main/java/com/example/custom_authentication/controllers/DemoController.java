18684c381ac2041870fdbef36a9f2145ae904197
Paula Rusti <rusti.paula78@gmail.com>
2023-05-16 18:40:14 +0300

    add demo endpoint

---
+++ custom_authentication/src/main/java/com/example/custom_authentication/controllers/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.custom_authentication.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/demo")
+    public String hello() {
+        return "Hello World!";
+    }
+}
