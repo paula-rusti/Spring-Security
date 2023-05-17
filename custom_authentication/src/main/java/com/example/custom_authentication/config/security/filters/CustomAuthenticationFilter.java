265637ee6c5211c4fa00d9b0ab3dc447e899a055
Paula Rusti <rusti.paula78@gmail.com>
2023-05-17 16:02:56 +0300

    added skeleton for auth with custom auth manager and provider

--- custom_authentication/src/main/java/com/example/custom_authentication/config/security/filters/CustomAuthenticationFilter.java
+++ custom_authentication/src/main/java/com/example/custom_authentication/config/security/filters/CustomAuthenticationFilter.java
@@ -1,8 +1,13 @@
 package com.example.custom_authentication.config.security.filters;
 
+import com.example.custom_authentication.config.security.authentication.CustomAuthentication;
+import com.example.custom_authentication.config.security.managers.CustomAuthenticationManager;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,13 +15,33 @@ import java.io.IOException;
 
 
 @Component
+@AllArgsConstructor
 // make sure that the filter is only once called
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
+
+    private final CustomAuthenticationManager customAuthenticationManager;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+
+        // 1. create an authentication object which is not yet authenticated
+        // 2. delegate the authentication object to the authentication manager
+        // 3. get back the authentication from the manager
+        // 4. if the object is authenticated then send request to the next filter in the chain
+
+        String key = String.valueOf(request.getHeader("key"));
+        CustomAuthentication ca = new CustomAuthentication(false, key);
+
+
+        Authentication a = customAuthenticationManager.authenticate(null);
+
+        if (a.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(a);
+            filterChain.doFilter(request, response);
+        }
+
         filterChain.doFilter(request, response);    // propagate the request to the next filter iff auth worked
     }
 }
